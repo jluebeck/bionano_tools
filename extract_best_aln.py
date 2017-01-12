3a58f90c682a9881e4aba05f932460e19ed25b5b
Unknown <jluebeck@eng.ucsd.edu>
2017-01-11 16:10:59 -0800

    added multi contig functionality
    can get all the best alignents per contig

--- extract_best_aln.py
+++ extract_best_aln.py
@@ -13,23 +13,37 @@ import sys
 import os
 import argparse
 
-def find_best_aln(xmapF,xmapF_out):
+def find_best_aln(xmapF,xmapF_out,eachMap):
 	bestScore = 0
 	scoreToXmapID = {}
+	xmapIDToBestScore = {}
+	xmapIDToBestLine = {}
+
 	with open(xmapF) as infile, open(xmapF_out,'w') as outfile:
 		for line in infile:
 			if line.startswith("#h"):
 				head = line.rstrip().rsplit("\t")
+				head[0] = head[0].rsplit("#h ")[1]
 
 			if not line.startswith("#"):
 				fields = line.rsplit("\t")
 				fDict = dict(zip(head,fields))
 				currScore = float(fDict['Confidence'])
+				currQueryID = fDict['QryContigID']
+
 				if currScore > bestScore:
 					bestScore = currScore
 
 				if currScore not in scoreToXmapID:
 					scoreToXmapID[currScore] = set()
+
+				if currQueryID not in xmapIDToBestScore:
+					xmapIDToBestScore[currQueryID] = 0
+
+				if currScore > xmapIDToBestScore[currQueryID]:
+					xmapIDToBestScore[currQueryID] = currScore
+					xmapIDToBestLine[currQueryID] = set()
+					xmapIDToBestLine[currQueryID].add(line)
 
 				scoreToXmapID[currScore].add(line)
 
@@ -37,8 +51,15 @@ def find_best_aln(xmapF,xmapF_out):
 				outfile.write(line)
 
 		if scoreToXmapID:
-			for line in scoreToXmapID[bestScore]:
-				outfile.write(line)
+			if not eachMap:
+				for line in scoreToXmapID[bestScore]:
+					outfile.write(line)
+
+			else:
+				for queryID_lines in xmapIDToBestLine.values():
+					for line in queryID_lines:
+						outfile.write(line)
+
 
 
 if __name__ == '__main__':
@@ -46,11 +67,12 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Extract best alignment from XMAP file")
 	parser.add_argument("-i", "--input", help="name of input file",required=True)
 	parser.add_argument("-o", "--outname", help="extracted file name (default to [inputname]_BESTALN.xmap")
+	parser.add_argument("--each_query",help="extract the best alignment for each query CMAP aligned, default false",action='store_true')
 	args = parser.parse_args()
 
 	outname = args.outname
 	if not outname:
 		outname = args.input[:args.input.index(".xmap")] + "_BESTALN" + args.input[args.input.index(".xmap"):]
 
-	find_best_aln(args.input,outname)
+	find_best_aln(args.input,outname,args.each_query)
  
